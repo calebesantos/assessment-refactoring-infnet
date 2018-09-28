2adc71a931f616c94174885437353e414596c926
Calebe Santos <calebe.bsantos@outlook.com>
2018-09-28 05:21:39 -0300

    Extract method
    Long method - O método statement é muito longo contendo mais de uma responsabilidade.
    Para isso foi extraído em outros métodos com menores responsabilidades.

--- src/main/java/GildedRose/GildedRose.java
+++ src/main/java/GildedRose/GildedRose.java
@@ -11,11 +11,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                extracted(i);
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -43,11 +39,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        extracted(i);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
@@ -59,4 +51,12 @@ class GildedRose {
             }
         }
     }
+
+	private void extracted(int i) {
+		if (items[i].quality > 0) {
+		    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+		        items[i].quality = items[i].quality - 1;
+		    }
+		}
+	}
 }
