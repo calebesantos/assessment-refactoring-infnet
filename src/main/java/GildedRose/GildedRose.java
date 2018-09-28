8c73b305236bf33b1da9f312c0e4167f2d56d9e3
Calebe Santos <calebe.bsantos@outlook.com>
2018-09-28 06:16:30 -0300

    Move method
    O máu cheiro mais em evidência é o Feature Envy, a necessidade que havia da classe GildedRose depender do Item era muito alta!

--- src/main/java/GildedRose/GildedRose.java
+++ src/main/java/GildedRose/GildedRose.java
@@ -11,84 +11,9 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
 	        Item item = items[i];
 
-            updateQualityByName(item);
-            
-            updateSellIn(item);
-            updateQualityToNormalize(item);
+	        item.updateQualityByName();
+	        item.updateSellIn();
+	        item.updateQualityToNormalize();
         }
     }
-
-	private void updateQualityToNormalize(Item item) {
-		if (item.sellIn < 0) {
-		    if (!isAgedBrie(item)) {
-		        if (!isBackstagePassesToAConcert(item)) {
-		            decreaseQualityItem(item);
-		        } else {
-		            item.quality = item.quality - item.quality;
-		        }
-		    } else {
-		        increaseQualityItem(item);
-		    }
-		}
-	}
-
-	private void updateSellIn(Item item) {
-		if (!isSulfurasHandOfRagnaros(item)) {
-			item.sellIn = item.sellIn - 1;
-		}
-	}
-
-	private void updateQualityByName(Item item) {
-		if (!isAgedBrie(item) && !isBackstagePassesToAConcert(item)) {
-		    decreaseQualityItem(item);
-		} else {
-		    if (isInQualityLimitToIncrease(item)) {
-				item.quality = item.quality + 1;
-
-		        if (isBackstagePassesToAConcert(item)) {
-		            if (item.sellIn < 11) {
-		                increaseQualityItem(item);
-		            }
-
-		            if (item.sellIn < 6) {
-		                increaseQualityItem(item);
-		            }
-		        }
-		    }
-		}
-	}
-
-	private boolean isSulfurasHandOfRagnaros(Item item) {
-		return isNameEquals(item, "Sulfuras, Hand of Ragnaros");
-	}
-
-	private boolean isBackstagePassesToAConcert(Item item) {
-		return isNameEquals(item, "Backstage passes to a TAFKAL80ETC concert");
-	}
-
-	private boolean isAgedBrie(Item item) {
-		return isNameEquals(item, "Aged Brie");
-	}
-
-	private boolean isInQualityLimitToIncrease(Item item) {
-		return item.quality < 50;
-	}
-
-	private boolean isNameEquals(Item item, String valueToCompare) {
-		return item.name.equals(valueToCompare);
-	}
-
-	private void increaseQualityItem(Item item) {
-		if (isInQualityLimitToIncrease(item)) {
-		    item.quality = item.quality + 1;
-		}
-	}
-
-	private void decreaseQualityItem(Item item) {
-		if (item.quality > 0) {
-		    if (!isSulfurasHandOfRagnaros(item)) {
-		        item.quality = item.quality - 1;
-		    }
-		}
-	}
 }
--- src/main/java/GildedRose/Item.java
+++ src/main/java/GildedRose/Item.java
@@ -2,20 +2,94 @@ package GildedRose;
 
 public class Item {
 
-    public String name;
+	public String name;
 
-    public int sellIn;
+	public int sellIn;
 
-    public int quality;
+	public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+	public Item(String name, int sellIn, int quality) {
+		this.name = name;
+		this.sellIn = sellIn;
+		this.quality = quality;
+	}
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+	public void updateQualityToNormalize() {
+		if (this.sellIn < 0) {
+		    if (!isAgedBrie()) {
+		        if (!isBackstagePassesToAConcert()) {
+		        	this.decreaseQualityItem();
+		        } else {
+		        	this.quality = this.quality - this.quality;
+		        }
+		    } else {
+		        increaseQualityItem();
+		    }
+		}
+	}
+
+	public void updateSellIn() {
+		if (!isSulfurasHandOfRagnaros()) {
+			this.sellIn = this.sellIn - 1;
+		}
+	}
+
+	public void updateQualityByName() {
+		if (!isAgedBrie() && !isBackstagePassesToAConcert()) {
+		    decreaseQualityItem();
+		} else {
+		    if (isInQualityLimitToIncrease()) {
+				this.quality = this.quality + 1;
+
+		        if (isBackstagePassesToAConcert()) {
+		            if (this.sellIn < 11) {
+		                increaseQualityItem();
+		            }
+
+		            if (this.sellIn < 6) {
+		                increaseQualityItem();
+		            }
+		        }
+		    }
+		}
+	}
+
+	public void increaseQualityItem() {
+		if (isInQualityLimitToIncrease()) {
+			this.quality = this.quality + 1;
+		}
+	}
+
+	public void decreaseQualityItem() {
+		if (this.quality > 0) {
+			if (!isSulfurasHandOfRagnaros()) {
+				this.quality = this.quality - 1;
+			}
+		}
+	}
+
+	private boolean isInQualityLimitToIncrease() {
+		return this.quality < 50;
+	}
+
+	private boolean isSulfurasHandOfRagnaros() {
+		return isNameEquals("Sulfuras, Hand of Ragnaros");
+	}
+
+	private boolean isBackstagePassesToAConcert() {
+		return isNameEquals("Backstage passes to a TAFKAL80ETC concert");
+	}
+
+	private boolean isAgedBrie() {
+		return isNameEquals("Aged Brie");
+	}
+
+	private boolean isNameEquals(String valueToCompare) {
+		return this.name.equals(valueToCompare);
+	}
+
+	@Override
+	public String toString() {
+		return this.name + ", " + this.sellIn + ", " + this.quality;
+	}
 }
