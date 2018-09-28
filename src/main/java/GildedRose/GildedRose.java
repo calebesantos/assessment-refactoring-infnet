51da8d6954b327586cab46d67fa5c8d3c6de0286
Calebe Santos <calebe.bsantos@outlook.com>
2018-09-28 05:34:37 -0300

    Decompose conditional
    Um dos maus cheiros evidentes é o Duplicate code, pois é possível enxergar que uma determinada condição era feita diversas vezes além do Complex Conditional gerado.

--- src/main/java/GildedRose/GildedRose.java
+++ src/main/java/GildedRose/GildedRose.java
@@ -9,52 +9,60 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                extracted(i);
+            if (!isNameEquals(i, "Aged Brie")
+                    && !isNameEquals(i, "Backstage passes to a TAFKAL80ETC concert")) {
+                decreaseQualityItem(i);
             } else {
-                if (items[i].quality < 50) {
+                if (isInQualityLimitToIncrease(i)) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isNameEquals(i, "Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQualityItem(i);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQualityItem(i);
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!isNameEquals(i, "Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        extracted(i);
+                if (!isNameEquals(i, "Aged Brie")) {
+                    if (!isNameEquals(i, "Backstage passes to a TAFKAL80ETC concert")) {
+                        decreaseQualityItem(i);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    increaseQualityItem(i);
                 }
             }
         }
     }
 
-	private void extracted(int i) {
+	private boolean isInQualityLimitToIncrease(int i) {
+		return items[i].quality < 50;
+	}
+
+	private boolean isNameEquals(int i, String valueToCompare) {
+		return items[i].name.equals(valueToCompare);
+	}
+
+	private void increaseQualityItem(int i) {
+		if (isInQualityLimitToIncrease(i)) {
+		    items[i].quality = items[i].quality + 1;
+		}
+	}
+
+	private void decreaseQualityItem(int i) {
 		if (items[i].quality > 0) {
-		    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+		    if (!isNameEquals(i, "Sulfuras, Hand of Ragnaros")) {
 		        items[i].quality = items[i].quality - 1;
 		    }
 		}
